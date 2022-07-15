e48101543b961f342dda8b8df0937e03505a7ea5
Kenneth Albert <gichuka@gmail.com>
2022-07-15 21:09:40 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React,{ useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
+
+
 function App() {
+
+  const [appClass, setAppClass] = useState(true);
+  
+
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  //console.log((appClass = false ? "App dark" : "App light"))
+  function handleToggle() {
+    
+    setAppClass(!appClass);
+    
+  }
+  
 
   return (
-    <div className={appClass}>
+    <div className={appClass?"App light":"App dark" }>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleToggle}>{appClass?"Light Mode":"Dark Mode" }</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React,{ useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const[category,setCategory]=useState("All");
+
+ const newItems= items.filter(item=>{
+ return category==="All"?true:category===item.category
+  })
+
+ // console.log(newItems)
+
+ function handleFilter(e) {
+  setCategory(e.target.value)
+  
+ }
+ //console.log(category)
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleFilter}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +26,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {newItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
